0e1b871066a1a125c73774fb37784946ec495fb9
knezevickris <kristinaknezevic06@gmail.com>
2024-08-17 19:38:43 +0200

    gasss
    idemooo

---
+++ floydWarshall/floydWarshall.cpp
@@ -0,0 +1,15 @@
+﻿#include<omp.h>
+#include<stdio.h>
+
+#define N 10
+
+int main() {
+	int dimenzijaMatrice;
+	/*
+	* Neophodno je da korisnik unese broj čvorova grafa. Broj čvorova određuje dimenziju kvadratne matrice susjedsta čvorova.
+	*/
+	printf("Unesite broj čvorova grafa: ");
+	scanf("%d", &dimenzijaMatrice);
+	printf("dimenzija matrice je %d", dimenzijaMatrice);
+	int matricaUdaljenosti[N][N] = {};
+}
